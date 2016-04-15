d06960bf8e1fe69bcaee075307a5a5db9d435630
kevin-kang <kang406864868@126.com>
2016-04-16 00:43:12 +0800

    更新Object.assign不能转为es5语法

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -92,6 +92,7 @@ module.exports = {
                 loader: 'babel',
                 query: {
                     presets: ['es2015'],
+                    plugins: ['transform-runtime'],
                     sourceMaps: 'both'
                 }
             },
