fb40240d6b42f4e4d14c8c04758ca50c1bec1e4c
Kevinkang <kang406864868@126.com>
2016-04-30 11:04:49 +0800

    更新gulpfile配置文件，可以根据不不同参数和目录名称打包环境。增加一个config.json配置文件，主要是针对API接口处于不同环境时路径不同。 例如： 开发环境 gulp --dev pc 指pc目录下打包执行dev命令  开发时请用该命令。以下命令为上线各个不同环境时执行 测试环境 gulp --test pc 准生产环境 gulp --repro pc 生产环境 gulp --pro pc

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,26 +4,86 @@ import webpack from 'webpack';
 import connect from 'gulp-connect';
 import plumber from 'gulp-plumber';
 import changed from 'gulp-changed';
-import webpackConfig from './webpack.config.js';
+import apiUrl from './config.json'; //配置不同环境下的API接口 默认dev ->本地环境，test ->测试环境，repro ->准生成环境，pro ->生成环境
+import opt from 'minimist';  //获取命令行参数
 
-let devPath = 'src', //开发目录
-    assets = 'dest', //上线目录
-    paths = [`${devPath}/*.html`, `${devPath}/**/*.+(scss|js|jpg|gif|png|svg)`];//文件路劲
 
-webpackConfig.output.path = assets;//设置webpackconfig输出路径
+let args = process.argv.slice(2),
+    argsOpt = opt(args),
+    basePaths = `${args[2]}` == 'undefined' ? `${args[1]}` : `${args[2]}`,
+    webpackConfig = require(`./webpack.config/webpack.config.${basePaths}.babel.js`), //根据gulp参数获取不同webpack配置文件 注意：此webpack文件是用ES6导出，故会多一个default属性
+    devPath = `src/${basePaths}`, //开发目录
+    dest = `dest/${basePaths}`, //本地编译后目录
+    testPath = `test/${basePaths}`, //测试环境目录
+    reproPath = `repro/${basePaths}`, //准生成环境目录
+    proPath = `pro/${basePaths}`, //生成环境目录
+    assets = dest, //默认目录为本地dest目录
+    paths = [`${devPath}/*.html`, `${devPath}/**/*.+(scss|js|jpg|gif|png|svg)`, `!${devPath}/**/config.js`]; //文件路劲
 
-webpackConfig.output.publicPath = `/${assets}/`;//设置webpackconfig公共输出路径
+
+let string_src = (filename, string) => { //把特定的字符转成流
+    let src = require('stream').Readable({
+        objectMode: true
+    })
+    src._read = function() {
+        this.push(new gutil.File({
+            cwd: '',
+            base: '',
+            path: filename,
+            contents: new Buffer(string)
+        }));
+        this.push(null);
+    }
+    return src;
+};
+
+if(argsOpt.test){
+   assets =  testPath;
+}
+if(argsOpt.repro){
+    assets =  reproPath;
+}
+if(argsOpt.pro){
+    assets =  proPath;
+}
+
+webpackConfig.default.output.path = assets; //设置webpackconfig输出路径
+
+webpackConfig.default.output.publicPath = argsOpt.test || argsOpt.repro || argsOpt.pro ? `/` : `/${assets}/`; //设置webpackconfig公共输出路径
+
+gulp.task('constants', () => {
+    //取出对应的配置信息
+    let envConfig, conConfig;
+
+    if(argsOpt.test){
+        envConfig = apiUrl.test;
+    }else if(argsOpt.repro){
+        envConfig = apiUrl.repro;
+    }else if(argsOpt.pro){
+        envConfig = apiUrl.pro;
+    }else {
+        envConfig = apiUrl.dev;
+    }
+
+    conConfig = `let baseUrl = { url: ${JSON.stringify(envConfig)}}; export default baseUrl;`;
+
+    //生成config.js文件
+    return string_src('config.js', conConfig)
+        .pipe(gulp.dest(`${devPath}/js/`));
+});
 
 //监听文件变化且自动刷新文件
 gulp.task('watch', () => {
-    gulp.watch(paths, ['livereload'], event => {
+    let watcher = gulp.watch(paths, ['livereload']);
+
+    watcher.on('change', event => {
         console.log(`File ${event.path} was ${event.type}, running tasks...`);
-    });
+    })
 });
 
 //自动刷新任务
 gulp.task('livereload', ['copy'], () => {
-    gulp.src(paths)
+    return gulp.src(paths)
         //'changed' 任务需要提前知道目标目录位置
         // 才能找出哪些文件是被修改过的
         .pipe(changed(assets))
@@ -33,15 +93,17 @@ gulp.task('livereload', ['copy'], () => {
 
 //copy开发目录下的images文件夹 先执行webpack，再执行copy
 gulp.task('copy', ['webpack'], () => {
-    return gulp.src(`${devPath}/images/**`)
+    return gulp.src([`${devPath}/images/**`, `${devPath}/releasepackage/**`], {
+            base: `${devPath}`
+        })
         .pipe(changed(assets))
         .pipe(plumber())
-        .pipe(gulp.dest(assets +'/images'));
+        .pipe(gulp.dest(assets));
 });
 
 //webpack打包任务
 gulp.task('webpack', cb => {
-    return webpack(webpackConfig, (err, stats) => {
+    return webpack(webpackConfig.default, (err, stats) => {
         if (err) throw new gutil.PluginError('webpack', err);
         gutil.log("[webpack]", stats.toString({
             colors: true
@@ -57,7 +119,14 @@ gulp.task('server', () => {
     });
 });
 
-// 运行任务
-gulp.task('default', () =>{
-    gulp.start('server','watch');
+// 运行默认任务 
+// gulp --dev mobile 第一个参数为打包开发环境，第二个参数为输入输出的目录
+gulp.task('default', ['constants'], () => {
+    gulp.start('server', 'watch');
+});
+
+//打包测试，准生成，生产环境 --test, --repro, --pro 三个参数
+//gulp allpack --test mobile 第一个参数为打包测试环境，第二个参数为要打包的目录
+gulp.task('allpack', ['constants'], () => {
+    gulp.start('copy');
 });
