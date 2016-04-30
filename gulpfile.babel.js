b45d99c0b7ab61d4e66f3cf8a8be087814be9d7c
Kevinkang <kang406864868@126.com>
2016-04-30 21:36:58 +0800

    增加help命令解释各命令的含义及用法

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,13 +5,12 @@ import connect from 'gulp-connect';
 import plumber from 'gulp-plumber';
 import changed from 'gulp-changed';
 import apiUrl from './config.json'; //配置不同环境下的API接口 默认dev ->本地环境，test ->测试环境，repro ->准生成环境，pro ->生成环境
-import opt from 'minimist';  //获取命令行参数
-
+import opt from 'minimist'; //获取命令行参数
 
 let args = process.argv.slice(2),
     argsOpt = opt(args),
     basePaths = `${args[2]}` == 'undefined' ? `${args[1]}` : `${args[2]}`,
-    webpackConfig = require(`./webpack.config/webpack.config.${basePaths}.babel.js`), //根据gulp参数获取不同webpack配置文件 注意：此webpack文件是用ES6导出，故会多一个default属性
+    webpackConfig = require(`./webpack.config/webpack.config.${basePaths}.babel.js`), //根据gulp参数获取不同webpack配置文件
     devPath = `src/${basePaths}`, //开发目录
     dest = `dest/${basePaths}`, //本地编译后目录
     testPath = `test/${basePaths}`, //测试环境目录
@@ -19,7 +18,6 @@ let args = process.argv.slice(2),
     proPath = `pro/${basePaths}`, //生成环境目录
     assets = dest, //默认目录为本地dest目录
     paths = [`${devPath}/*.html`, `${devPath}/**/*.+(scss|js|jpg|gif|png|svg)`, `!${devPath}/**/config.js`]; //文件路劲
-
 
 let string_src = (filename, string) => { //把特定的字符转成流
     let src = require('stream').Readable({
@@ -37,14 +35,14 @@ let string_src = (filename, string) => { //把特定的字符转成流
     return src;
 };
 
-if(argsOpt.test){
-   assets =  testPath;
+if (argsOpt.test) {
+    assets = testPath;
 }
-if(argsOpt.repro){
-    assets =  reproPath;
+if (argsOpt.repro) {
+    assets = reproPath;
 }
-if(argsOpt.pro){
-    assets =  proPath;
+if (argsOpt.pro) {
+    assets = proPath;
 }
 
 webpackConfig.default.output.path = assets; //设置webpackconfig输出路径
@@ -55,13 +53,13 @@ gulp.task('constants', () => {
     //取出对应的配置信息
     let envConfig, conConfig;
 
-    if(argsOpt.test){
+    if (argsOpt.test) {
         envConfig = apiUrl.test;
-    }else if(argsOpt.repro){
+    } else if (argsOpt.repro) {
         envConfig = apiUrl.repro;
-    }else if(argsOpt.pro){
+    } else if (argsOpt.pro) {
         envConfig = apiUrl.pro;
-    }else {
+    } else {
         envConfig = apiUrl.dev;
     }
 
@@ -78,17 +76,19 @@ gulp.task('watch', () => {
 
     watcher.on('change', event => {
         console.log(`File ${event.path} was ${event.type}, running tasks...`);
-    })
+    });
 });
 
 //自动刷新任务
 gulp.task('livereload', ['copy'], () => {
+    gulp.start('help');
     return gulp.src(paths)
         //'changed' 任务需要提前知道目标目录位置
         // 才能找出哪些文件是被修改过的
         .pipe(changed(assets))
         .pipe(plumber())
         .pipe(connect.reload());
+
 });
 
 //copy开发目录下的images文件夹 先执行webpack，再执行copy
@@ -122,11 +122,21 @@ gulp.task('server', () => {
 // 运行默认任务 
 // gulp --dev mobile 第一个参数为打包开发环境，第二个参数为输入输出的目录
 gulp.task('default', ['constants'], () => {
-    gulp.start('server', 'watch');
+    gulp.start('server', 'watch', 'help');
 });
 
 //打包测试，准生成，生产环境 --test, --repro, --pro 三个参数
 //gulp allpack --test mobile 第一个参数为打包测试环境，第二个参数为要打包的目录
 gulp.task('allpack', ['constants'], () => {
     gulp.start('copy');
+});
+
+gulp.task('help', () => {
+    console.log('++++++++++++++++++++++++++++++++');
+    console.log('xxx代表目录名');
+    console.log('gulp --dev xxx 开启本地静态服务器和自动刷新功能以及打包');
+    console.log('gulp --test xxx 测试服务器打包');
+    console.log('gulp --repro xxx 准生产服务器打包');
+    console.log('gulp --pro xxx 生产服务器打包');
+    console.log('++++++++++++++++++++++++++++++++');
 });
